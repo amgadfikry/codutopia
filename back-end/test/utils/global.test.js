70da5b1764077a4dd1e064d8c0b85daf7ecd4ec1
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-02-21 04:30:09 +0200

    modify old test accord modified methods and add new test to new classes and new method

---
+++ back-end/test/utils/global.test.js
@@ -0,0 +1,29 @@
+import Global from "../../server/utils/global.js";
+import { expect } from "chai";
+import sinon from "sinon";
+
+// unit tests for the Global class methods
+describe("Unittest of Global methods", () => {
+  // Test suite for the prepareDataToRedis method
+  describe('PrepareDataToRedis method', () => {
+    // Test case for prepareDataToRedis method
+    it('when provided data contain password, _id, list', () => {
+      const data = {
+        _id: "60f3e3e3e3e3e3e3e3e3e3e3",
+        email: "email",
+        role: "user",
+        userName: "userName",
+        list: ["one", "two", "three"],
+        password: 'password'
+      };
+      const result = Global.prepareDataToRedis(data);
+      expect(result).to.deep.equal({
+        id: "60f3e3e3e3e3e3e3e3e3e3e3",
+        email: "email",
+        role: "user",
+        userName: "userName",
+        list: JSON.stringify(data.list)
+      });
+    });
+  });
+});

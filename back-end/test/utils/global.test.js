bed33fdf60e566587dcb7c56447c3ecc57f41764
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-02-22 00:15:19 +0200

    arrange all test cases in one test suit

--- back-end/test/utils/global.test.js
+++ back-end/test/utils/global.test.js
@@ -4,26 +4,23 @@ import sinon from "sinon";
 
 // unit tests for the Global class methods
 describe("Unittest of Global methods", () => {
-  // Test suite for the prepareDataToRedis method
-  describe('PrepareDataToRedis method', () => {
-    // Test case for prepareDataToRedis method
-    it('when provided data contain password, _id, list', () => {
-      const data = {
-        _id: "60f3e3e3e3e3e3e3e3e3e3e3",
-        email: "email",
-        role: "user",
-        userName: "userName",
-        list: ["one", "two", "three"],
-        password: 'password'
-      };
-      const result = Global.prepareDataToRedis(data);
-      expect(result).to.deep.equal({
-        id: "60f3e3e3e3e3e3e3e3e3e3e3",
-        email: "email",
-        role: "user",
-        userName: "userName",
-        list: JSON.stringify(data.list)
-      });
+  // Test case for prepareDataToRedis method
+  it('when provided data contain password, _id, list', () => {
+    const data = {
+      _id: "60f3e3e3e3e3e3e3e3e3e3e3",
+      email: "email",
+      role: "user",
+      userName: "userName",
+      list: ["one", "two", "three"],
+      password: 'password'
+    };
+    const result = Global.prepareDataToRedis(data);
+    expect(result).to.deep.equal({
+      id: "60f3e3e3e3e3e3e3e3e3e3e3",
+      email: "email",
+      role: "user",
+      userName: "userName",
+      list: JSON.stringify(data.list)
     });
   });
 });

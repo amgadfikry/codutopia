2f0f9c2466c7b73d67eb3ee6ba527d4289974482
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-02-23 16:06:53 +0200

    modify authmiddleware to convert data from mongo to stringify to save in redis

--- back-end/server/controllers/middlewareControl.js
+++ back-end/server/controllers/middlewareControl.js
@@ -1,5 +1,6 @@
 import redisDB from "../../databases/redisDB.js";
 import mongoDB from "../../databases/mongoDB.js";
+import Global from "../utils/global.js";
 import jwt from "jsonwebtoken";
 import { ObjectId } from "mongodb";
 
@@ -17,38 +18,33 @@ class MiddlewareControl {
     try {
       // Check if the token exists in the header
       const token = req.headers.authorization;
-      if (token) {
-        // Get the user from redis
-        const user = await redisDB.getHashAll(token);
-        if (user) {
-          // Set the user in res.locals and call the next middleware
-          res.locals.user = user;
-          res.locals.token = token;
-          return next();
-        }
-        // Verify the token
-        const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-        if (verifyToken) {
-          // Get the user from mongoDB and set the user in redis
-          const gettingUser = await mongoDB.getOne(verifyToken.role, { _id: new ObjectId(verifyToken.id) });
-          if (gettingUser) {
-            const data = {
-              email: gettingUser.email,
-              fullName: gettingUser.fullName,
-              userName: gettingUser.userName,
-              id: verifyToken.id,
-              role: gettingUser.role,
-            }
-            await redisDB.setHashMulti(token, data, 259200);
-            // Set the user in res.locals and call the next middleware
-            res.locals.user = data;
-            res.locals.token = token;
-            return next();
-          }
-        }
+      if (!token) {
+        return res.status(401).json({ msg: "Unauthorized" });
       }
-      // 
-      return res.status(401).json({ msg: 'Unauthorized' });
+      // Get the user from redis
+      const user = await redisDB.getHashAll(token);
+      if (user) {
+        // Set the user in res.locals and call the next middleware
+        res.locals.user = user;
+        res.locals.token = token;
+        return next();
+      }
+      // Verify the token
+      const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+      if (!verifyToken) {
+        return res.status(401).json({ msg: "Unauthorized" });
+      }
+      // Get the user from mongoDB and set the user in redis
+      const gettingUser = await mongoDB.getOne(verifyToken.role, { _id: new ObjectId(verifyToken.id) });
+      if (!gettingUser) {
+        return res.status(401).json({ msg: "Unauthorized" });
+      }
+      const data = Global.prepareDataToRedis(gettingUser);
+      await redisDB.setHashMulti(token, data, 259200);
+      // Set the user in res.locals and call the next middleware
+      res.locals.user = data;
+      res.locals.token = token;
+      return next();
     } catch (error) {
       return res.status(500).json({ msg: 'Internal server error' });
     }

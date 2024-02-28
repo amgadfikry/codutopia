612743a19a2817630cf1d517cb432e5f2e62b2df
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-02-28 07:58:18 +0200

    modify some elements in landing page

--- front-end/src/pages/landingPage/supportSection.jsx
+++ front-end/src/pages/landingPage/supportSection.jsx
@@ -11,7 +11,7 @@ function SupportSection() {
         <p className="mt-4 text-dark-blue font-light text-center lg:font-normal">
           We are here to help you with any questions you have. Just send us a message and we will get back to you as soon as possible.
         </p>
-        <Link to='/support' className='mt-6'>
+        <Link to='/login' className='mt-6'>
           <button className="btn-dark-red mt-4 text-lg">
             Contact Support
           </button>

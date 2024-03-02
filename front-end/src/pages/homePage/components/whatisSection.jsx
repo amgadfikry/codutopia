4f1a2ed3e0e2481abcc1fb2d3534fc028c72dfda
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-03-02 09:39:44 +0200

    change the name of web app across the react app files

--- front-end/src/pages/homePage/components/whatisSection.jsx
+++ front-end/src/pages/homePage/components/whatisSection.jsx
@@ -5,7 +5,7 @@ function WhatisSection() {
       <div className="container mx-auto px-3">
         <h3 className="text-2xl text-darker-blue font-bold lg:text-3xl border-b-4 border-light-red w-fit
         pb-2 mb-10 mx-auto">
-          What is E-Learning Platform?
+          What is Codutopia Platform?
         </h3>
         <p className="mt-4 text-dark-blue font-light text-center lg:font-normal">
           Learn coding online and gain the skills you need to become a software engineer, data scientist,

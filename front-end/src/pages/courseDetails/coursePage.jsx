9b61e818fcbfef81329486b7e8b202b8dc3ba127
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-03-02 05:02:34 +0200

    add state to manage url of photo from file storage

--- front-end/src/pages/courseDetails/coursePage.jsx
+++ front-end/src/pages/courseDetails/coursePage.jsx
@@ -15,6 +15,7 @@ function CoursePage() {
   const [courseDetails, setCourseDetails] = useState({});
   const [lesson, setLesson] = useState(0);
   const [edit, setEdit] = useState(false);
+  const [imageUrls, setImageUrls] = useState('');
   const navigate = useNavigate();
   // get course id from url
   const { id } = useParams();
@@ -35,6 +36,7 @@ function CoursePage() {
         // set course details
         if (res.data.data) {
           setCourseDetails(res.data.data);
+          setImageUrls(`${url}/files/get/image/${res.data.data.image.objectKey}`);
           // calculate current lesson if auth is learner
           if (auth === 'learner' && userCourse) {
             const progress = userCourse.progress;
@@ -54,7 +56,7 @@ function CoursePage() {
 
   return (
     <section className='container mx-auto pt-24 pb-16 relative text-white px-2' >
-      <CourseHead courseDetails={courseDetails} auth={auth} userCourse={userCourse} />
+      <CourseHead courseDetails={courseDetails} auth={auth} userCourse={userCourse} imageUrls={imageUrls} />
       <Buttons auth={auth} userCourse={userCourse} lesson={lesson} id={id} edit={edit} setEdit={setEdit} />
       <Routes>
         <Route path="/" element={<CourseContent

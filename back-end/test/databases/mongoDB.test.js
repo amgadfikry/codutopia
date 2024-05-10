ef4cf3ff50bc720731adfba0174be04601ebf4bf
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-05-10 16:59:06 +0300

    Enhance test cases for mongodb methods - Add new test cases for update one document using diffrent operators - Use objectID of added docuemt in query in all test cases - Unified the age of added document in all test cases to use it in query - Enhannce docmentaion of test cases

--- back-end/test/databases/mongoDB.test.js
+++ back-end/test/databases/mongoDB.test.js
@@ -3,25 +3,29 @@ import { expect } from 'chai';
 
 // Test suite for the mongoDB module and its methods by connecting to the database
 describe('Unittest of MongoDB', () => {
-  //after all tests are finished remove the test collection
+  // variable to save id of the document
+  let id;
+
+  //after all tests are finished remove the test collection  
   after(async () => {
     await mongoDB.db.collection('test').drop();
   });
 
   //Test case when add new document to the collection
   it('AddOne add new document to collection', async () => {
-    const result = await mongoDB.addOne('test', { name: 'test', age: 20 });
+    const result = await mongoDB.addOne('test', { name: 'test0', age: 20 });
+    id = result;
+    // expect the result to be an objectID of the added document
     expect(result).to.be.an('object');
   });
 
   //Test case when add many documents to the collection 
   it('AddMany add many documents to collection', async () => {
-    const manyDocuments = [{ name1: 'test1', age: 20 }, { name2: 'test2', age: 20 },
-    { name3: 'test3', age: 20 }, { name4: 'test4', age: 20 }, { name5: 'test5', age: 20 },
-    { name6: 'test6', age: 20 }, { name7: 'test7', age: 20 }, { name8: 'test8', age: 20 },
-    { name9: 'test9', age: 20 }, { name10: 'test10', age: 20 }, { name11: 'test11', age: 20 },
-    { name12: 'test12', age: 20 }, { name13: 'test13', age: 20 }, { name14: 'test14', age: 20 },
-    { name15: 'test15', age: 20 }]
+    const manyDocuments = []
+    // add 15 documents to the collection with the same age
+    for (let i = 1; i < 16; i++) {
+      manyDocuments.push({ name: `test${i}`, age: 30 });
+    }
     const result = await mongoDB.addMany('test', manyDocuments);
     expect(result).to.be.an('object');
     expect(result.insertedCount).to.equal(15);
@@ -29,109 +33,133 @@ describe('Unittest of MongoDB', () => {
 
   //Test case when get one document from the collection
   it('GetOne get one document from collection successfully', async () => {
-    const result = await mongoDB.getOne('test', { name5: 'test5' });
+    // get the document by the id of the added document in the first test case
+    const result = await mongoDB.getOne('test', { _id: id });
     expect(result).to.be.an('object');
-    expect(result.name5).to.equal('test5');
-    expect(result._id).to.be.an('object');
+    expect(result).deep.equal({ _id: id, name: 'test0', age: 20 });
+    expect(result._id.toString()).to.equal(id.toString());
   });
 
   //Test case when get all documents from the collection
   it('GetAll when get all documents from collection ', async () => {
-    const result = await mongoDB.getAll('test', { age: 20 });
+    // get all documents from the collection using an empty query
+    const result = await mongoDB.getAll('test', {});
     expect(result).to.be.an('array');
     expect(result.length).to.equal(16);
   });
 
   //Test case when get a specific number of documents per page from the collection
   it('Pagination get a specific number of documents per page from collection', async () => {
-    const result = await mongoDB.pagination('test', { age: 20 }, 1, 10);
+    // get first 10 documents per page from the collection 
+    const result = await mongoDB.pagination('test', {}, 1, 10);
     expect(result).to.be.an('array');
     expect(result.length).to.equal(10);
-    const result2 = await mongoDB.pagination('test', { age: 20 }, 2, 10);
+    // get the second 10 documents per page from the collection
+    const result2 = await mongoDB.pagination('test', {}, 2, 10);
     expect(result2).to.be.an('array');
     expect(result2.length).to.equal(6);
   });
 
-  // Test case when get many documents from the collection in the list
+  // Test case when get many documents from the collection query by a list of values
   it('GetFromList documents get many documents from the collection in the list', async () => {
-    const result = await mongoDB.getFromList('test', 'name5', ['test5']);
-    expect(result).to.be.an('array');
-    expect(result.length).to.equal(1);
-    expect(result[0].name5).to.equal('test5');
+    // get documents from the collection by list of names
+    const result = await mongoDB.getFromList('test', 'name', ['test1', 'test2', 'test3', 'test4', 'test5']);
+    expect(result.length).to.equal(5);
+    expect(result[0].name).to.equal('test1');
   });
 
   // Test case when update one document in the collection using $set
-  it('UpdateOne update one document in the collection using $set', async () => {
-    const result = await mongoDB.updateOne('test', { name5: 'test5' }, { $set: { age: 25 } });
+  it('Using $set opeartor to update document', async () => {
+    // update the age of the document to 25
+    const result = await mongoDB.updateOne('test', { _id: id }, { $set: { age: 25 } });
     expect(result).to.be.an('object');
     expect(result.modifiedCount).to.equal(1);
-    const value = await mongoDB.getOne('test', { name5: 'test5' });
+    const value = await mongoDB.getOne('test', { _id: id });
     expect(value.age).to.equal(25);
   });
 
   // Test case when update one document in the collection using $inc
-  it('UpdateOne update one document in the collection using $inc', async () => {
-    const result = await mongoDB.updateOne('test', { name5: 'test5' }, { $inc: { age: 1 } });
+  it('Using $inc opeartor to update document', async () => {
+    // increment the age of the document by 1
+    const result = await mongoDB.updateOne('test', { _id: id }, { $inc: { age: 1 } });
     expect(result).to.be.an('object');
     expect(result.modifiedCount).to.equal(1);
-    const value = await mongoDB.getOne('test', { name5: 'test5' });
+    const value = await mongoDB.getOne('test', { _id: id });
     expect(value.age).to.equal(26);
   });
 
-  //Test case when update one document in the collection using $push
-  it('UpdateOne update one document in the collection using $push', async () => {
-    await mongoDB.updateOne('test', { name5: 'test5' }, { $set: { hobbies: [] } });
-    const result = await mongoDB.updateOne('test', { name5: 'test5' }, { $push: { hobbies: 'football' } });
+  // Test case when update one document in the collection using $unset
+  it('Using $unset opeartor to update document', async () => {
+    // remove the age field from the document
+    const result = await mongoDB.updateOne('test', { _id: id }, { $unset: { age: '' } });
     expect(result).to.be.an('object');
     expect(result.modifiedCount).to.equal(1);
-    const value = await mongoDB.getOne('test', { name5: 'test5' });
+    const value = await mongoDB.getOne('test', { _id: id });
+    expect(value.age).to.equal(undefined);
+  });
+
+  //Test case when update one document in the collection using $push
+  it('Using $push opeartor to update document', async () => {
+    // add a new hobby list to the document
+    await mongoDB.updateOne('test', { _id: id }, { $set: { hobbies: [] } });
+    // push a new hobby to the document hobbies list
+    const result = await mongoDB.updateOne('test', { _id: id }, { $push: { hobbies: 'football' } });
+    expect(result).to.be.an('object');
+    expect(result.modifiedCount).to.equal(1);
+    const value = await mongoDB.getOne('test', { _id: id });
     expect(value.hobbies).to.be.an('array');
     expect(value.hobbies.length).to.equal(1);
     expect(value.hobbies[0]).to.equal('football');
   });
 
   //Test case when update one document in the collection using $pull
-  it('UpdateOne update one document in the collection using $pull', async () => {
-    const result = await mongoDB.updateOne('test', { name5: 'test5' }, { $pull: { hobbies: 'football' } });
+  it('Using $pull opeartor to update document', async () => {
+    // pull the hobby from the document hobbies list
+    const result = await mongoDB.updateOne('test', { _id: id }, { $pull: { hobbies: 'football' } });
     expect(result).to.be.an('object');
     expect(result.modifiedCount).to.equal(1);
-    const value = await mongoDB.getOne('test', { name5: 'test5' });
+    const value = await mongoDB.getOne('test', { _id: id });
     expect(value.hobbies).to.be.an('array');
     expect(value.hobbies.length).to.equal(0);
   });
 
   // Test case of update many docuemts in the collection
   it('UpdateMany update many documents in the collection', async () => {
-    const result = await mongoDB.updateMany('test', { age: 20 }, { $set: { age: 30 } });
+    // update the age of all documents with age 30 to 35
+    const result = await mongoDB.updateMany('test', { age: 30 }, { $set: { age: 35 } });
     expect(result).to.be.an('object');
     expect(result.modifiedCount).to.equal(15);
-    const value = await mongoDB.getAll('test', { age: 30 });
+    const value = await mongoDB.getAll('test', { age: 35 });
     expect(value).to.be.an('array');
     expect(value.length).to.equal(15);
   });
 
   //Test case when delete one document from the collection
   it('DeleteOne delete one document from collection', async () => {
-    const result = await mongoDB.deleteOne('test', { name5: 'test5' });
+    // delete the document by the id of the added document in the first test case
+    const result = await mongoDB.deleteOne('test', { _id: id });
     expect(result).to.be.an('object');
     expect(result.deletedCount).to.equal(1);
-    const value = await mongoDB.getOne('test', { name5: 'test5' });
+    const value = await mongoDB.getOne('test', { _id: id });
     expect(value).to.equal(null);
   });
 
   //Test case when count documents from the collection
   it('Count count documents from collection', async () => {
-    const result = await mongoDB.countColl('test', { age: 30 });
+    // count the documents with age 35
+    const result = await mongoDB.countColl('test', { age: 35 });
     expect(result).to.be.a('number');
     expect(result).to.equal(15);
   });
 
   //Test case when delete many documents from the collection
   it('DeleteMany delete many documents from collection', async () => {
-    const result = await mongoDB.deleteMany('test', { age: 30 });
+    // delete all documents with age 35
+    const result = await mongoDB.deleteMany('test', { age: 35 });
     expect(result).to.be.an('object');
     expect(result.deletedCount).to.equal(15);
-    const value = await mongoDB.countColl('test', { age: 30 });
+    // count the documents with age 35
+    const value = await mongoDB.countColl('test', { age: 35 });
     expect(value).to.equal(0);
   });
 });

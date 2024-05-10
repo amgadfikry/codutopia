d102f1312ffc7d11774e282248776aef9eb94386
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-05-10 21:09:05 +0300

    Enchance documentation, methods and add new methods to the redis class - enhance the documentation of some methods - modify set method to accept time default value - modify setHashMulti method to can add time of expire for all keys - add new method keyExist to check if key exist or not - add new method fieldExists to check if field exist in hash or not

--- back-end/databases/redisDB.js
+++ back-end/databases/redisDB.js
@@ -4,29 +4,32 @@ import ioredis from 'ioredis';
 class RedisBD {
   /* constructor to connect to the database
     get the url from the environment variables provided by the .env file pass to server container
-    connect to the database and set the redis property to the database name
+    connect to the database and set the redis property to the database number
   */
   constructor() {
     let url = process.env.REDIS_URL || 'redis://localhost:6379';
     const envVar = process.env.NODE_ENV;
+    // database number 0 is the default database for production
     let db = 0;
     if (envVar === 'test') {
+      // database number 1 is the test database
       db = 1;
     } else if (envVar === 'dev') {
+      // database number 2 is the development database
       db = 2;
     }
-    url += `/${db}`;
-    this.redis = new ioredis(url);
+    this.redis = new ioredis(url, { db });
   }
 
-  /* methods to add one key-value pair to the database
+  /* methods to add one key-value pair to the database with an optional time to live
     Parameters:
     - key - the key
     - value - the value
+    - time - the time to live with the default value 0
     Returns:
     - the result of the operation
   */
-  async set(key, value, time) {
+  async set(key, value, time = 0) {
     try {
       const result = await this.redis.set(key, value);
       if (time) {
@@ -38,17 +41,17 @@ class RedisBD {
     }
   }
 
-  /* methods to add hash key-value pairs to the database
+  /* methods to add hash key-value pairs to the database with an optional time to live
     Parameters:
     - key - the key
-    - filedValues - the object with the key-value pairs
-    - time - the time to live
+    - keyValuePairs - the object with the key-value pairs
+    - time - the time to live with the default value 0
     Returns:
     - the result of the operation
   */
-  async setHashMulti(key, filedValues, time = 0) {
+  async setHashMulti(key, keyValuePairs, time = 0) {
     try {
-      const result = await this.redis.hmset(key, filedValues);
+      const result = await this.redis.hmset(key, keyValuePairs);
       if (time) {
         await this.redis.expire(key, time);
       }
@@ -111,6 +114,35 @@ class RedisBD {
   async delHashField(key, field) {
     try {
       return await this.redis.hdel(key, field);
+    } catch (e) {
+      return e;
+    }
+  }
+
+  /* Method to check if a key exists in the database
+    Parameters:
+    - key - the key
+    Returns:
+    - true if the key exists, false otherwise
+  */
+  async keyExists(key) {
+    try {
+      return await this.redis.exists(key);
+    } catch (e) {
+      return e;
+    }
+  }
+
+  /* Method to check if a field exists in the hash key-value pairs in the database
+    Parameters:
+    - key - the key
+    - field - the field
+    Returns:
+    - true if the field exists, false otherwise
+  */
+  async fieldExists(key, field) {
+    try {
+      return await this.redis.hexists(key, field);
     } catch (e) {
       return e;
     }

30ffad6340f61bb3b1eeadea7a1d6f170af1c35f
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-05-14 23:01:27 +0300

    Reconstruct OracleStorage class and it's methods - Save and import compartmentId and namespace from .env file - Add createBucket method to create a bucket in Oracle Cloud Storage - Add getAllBucket method to get all buckets in Oracle Cloud Storage - Add deleteBucket method to delete a bucket in Oracle Cloud Storage - Modify parameters names in createObj method - Add exists method to check if an object exists in a bucket - Change name and parameters names of get method to getObj - Add getAllObj method to get all objects in a bucket - Change name and parameters names of delete method to deleteObj

--- back-end/databases/oracleStorage.js
+++ back-end/databases/oracleStorage.js
@@ -1,73 +1,182 @@
 import common from 'oci-common';
 import objectStorage from 'oci-objectstorage';
 
-// Set up the authentication provider and the Object Storage client to use the Object Storage service.
+// OracleStorage class that provides methods to interact with the Object Storage service.
 class OracleStorage {
   constructor() {
-    // Set up the authentication provider and the Object Storage client to use the Object Storage service.
+    // Set up the authentication provider and region for the Object Storage client.
     this.config = {
       authenticationDetailsProvider: new common.ConfigFileAuthenticationDetailsProvider(
         './config.oci', 'DEFAULT'),
       region: 'me-jeddah-1',
+      logRequests: false,
     };
-    // Create an Object Storage client to use the Object Storage service.
+    // Create an instance of the Object Storage client using the configuration.
     this.objectStorageClient = new objectStorage.ObjectStorageClient(this.config);
+    this.namespace = process.env.NAMESPACE;
+    this.compartmentId = process.env.COMPARTMENT_ID;
   }
 
-  /* Uploads a file to the Object Storage service
-    * @param {string} type - The type of file to upload
-    * @param {string} originalname - The original name of the file
-    * @param {Buffer} buffer - The file content as a buffer
-    * @returns {Promise} - A promise that resolves to the result from the Object Storage service
-    */
-  async upload(type, originalname, buffer) {
+  /* CreateBucket method creates a new bucket in the Object Storage service.
+    Parameters:
+    - bucketName: The name of the bucket to create.
+    Returns:
+    - The result of the createBucket operation, which contains the metadata of the new bucket.
+  */
+  async createBucket(bucketName) {
     try {
-      const uploadObject = {
-        namespaceName: 'axtryshpute3',
-        bucketName: type,
-        objectName: originalname,
-        putObjectBody: buffer,
-        contentLength: buffer.length,
+      const createBucketRequest = {
+        namespaceName: this.namespace,
+        createBucketDetails: {
+          name: bucketName,
+          compartmentId: this.compartmentId,
+        }
       };
-      const result = await this.objectStorageClient.putObject(uploadObject);
+      const result = await this.objectStorageClient.createBucket(createBucketRequest);
       return result;
     } catch (err) {
       return err;
     }
   }
 
-  /* Retrieves a file from the Object Storage service
-    * @param {string} key - The name of the file to retrieve
-    * @param {string} type - The type of file to retrieve
-    * @returns {Promise} - A promise that resolves to the result from the Object Storage service
-    */
-  async get(key, type) {
+  /* GetAllBucket method retrieves all buckets from the Object Storage service.
+    Returns:
+    - The result of the listBuckets operation, which contains the metadata of the retrieved buckets.
+  */
+  async getAllBucket() {
     try {
-      const getObject = {
-        namespaceName: 'axtryshpute3',
-        bucketName: type,
-        objectName: key,
+      const getAllBucketDetails = {
+        namespaceName: this.namespace,
+        compartmentId: this.compartmentId,
       };
-      const result = await this.objectStorageClient.getObject(getObject);
+      const result = await this.objectStorageClient.listBuckets(getAllBucketDetails);
+      return result;
+    }
+    catch (err) {
+      return err;
+    }
+  }
+
+  /* DeleteBucket method deletes a bucket from the Object Storage service.
+    Parameters:
+    - bucketName: The name of the bucket to delete.
+    Returns:
+    - The result of the deleteBucket operation, which contains the metadata of the deleted bucket.
+  */
+  async deleteBucket(bucketName) {
+    try {
+      const deleteBucketDetails = {
+        namespaceName: 'axtryshpute3',
+        bucketName: bucketName,
+        compartmentId: this.compartmentId,
+      };
+      const result = await this.objectStorageClient.deleteBucket(deleteBucketDetails);
       return result;
     } catch (err) {
       return err;
     }
   }
 
-  /* Deletes a file from the Object Storage service
-    * @param {string} key - The name of the file to delete
-    * @param {string} type - The type of file to delete
-    * @returns {Promise} - A promise that resolves to the result from the Object Storage service
-    */
-  async delete(key, type) {
+  /* createObj method uploads an object to the Object Storage service.
+    Parameters:
+    - bucketName: The name of the bucket to upload the object to.
+    - objName: The name of the object to upload.
+    - data: The data to upload as the object content.
+    Returns:
+    - The result of the putObject operation, which contains the metadata of the uploaded object.
+  */
+  async createObj(bucketName, objName, data) {
     try {
-      const deleteObject = {
+      const createdObjectDetails = {
         namespaceName: 'axtryshpute3',
-        bucketName: type,
-        objectName: key,
+        bucketName: bucketName,
+        objectName: objName,
+        putObjectBody: data,
+        contentLength: data.length,
       };
-      const result = await this.objectStorageClient.deleteObject(deleteObject);
+      const result = await this.objectStorageClient.putObject(createdObjectDetails);
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  /*Existing methods check if an object exists or not
+  Parameters:
+  - objName: The name of the object to check.
+  - bucketName: The name of the bucket to search for the object.
+  Returns:
+  - The result of the headObject operation, which contains the metadata of the object if it exists.
+*/
+  async exists(bucketName, objName) {
+    try {
+      const objectDetails = {
+        namespaceName: 'axtryshpute3',
+        bucketName: bucketName,
+        objectName: objName,
+      };
+      const result = await this.objectStorageClient.headObject(objectDetails);
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  /* getObj method retrieves an object from the Object Storage service.
+    Parameters:
+    - bucketName: The name of the bucket to retrieve the object from.
+    - objName: The name of the object to retrieve.
+    Returns:
+    - The result of the getObject operation, which contains the metadata and data of the retrieved object.
+  */
+  async getObj(bucketName, objName) {
+    try {
+      const getObjectDetails = {
+        namespaceName: 'axtryshpute3',
+        bucketName: bucketName,
+        objectName: objName,
+      };
+      const result = await this.objectStorageClient.getObject(getObjectDetails);
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  /* GetAllObj method retrieves all objects from bucket in the Object Storage service.
+    Parameters:
+    - bucketName: The name of the bucket to retrieve the object from.
+    Returns:
+    - The result of the listObjects operation, which contains the metadata and data of the retrieved object.
+  */
+  async getAllObj(bucketName) {
+    try {
+      const listObjectsDetails = {
+        namespaceName: 'axtryshpute3',
+        bucketName: bucketName,
+      };
+      const result = await this.objectStorageClient.listObjects(listObjectsDetails);
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  /* delete method deletes an object from the Object Storage service.
+    Parameters:
+    - objName: The name of the object to delete.
+    - bucketName: The name of the bucket to delete the object from.
+    Returns:
+    - The result of the deleteObject operation, which contains the metadata of the deleted object.
+  */
+  async deleteObj(bucketName, objName) {
+    try {
+      const deleteObjectDetails = {
+        namespaceName: 'axtryshpute3',
+        bucketName: bucketName,
+        objectName: objName,
+      };
+      const result = await this.objectStorageClient.deleteObject(deleteObjectDetails);
       return result;
     } catch (err) {
       return err;

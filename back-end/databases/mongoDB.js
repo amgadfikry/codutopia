2ed92a71b5f0b69482d788a31b6d7ca6a3c5e0cf
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-06-08 07:40:55 +0300

    Add new method for transaction and add replication for mongoDB - Change workEnv to dev and production only - Add name of replicaSet for mongoDB - Pass replicaSet to mongoDB connection - Add three methods for using in transaction (startTransaction, commitTransaction, abortTransaction) - Remove unnecessary documentenation and comments

--- back-end/databases/mongoDB.js
+++ back-end/databases/mongoDB.js
@@ -3,19 +3,21 @@ import UserModel from './models/userModel.js';
 import LessonModel from './models/lessonModel.js';
 import QuizModel from './models/quizModel.js';
 import PaymentModel from './models/paymentModel.js';
+import CourseModel from './models/courseModel.js';
 
-// MongoDB class represents the connection to the database and the methods to interact with it
+// MongoDB class represents the connection to the database and the methods to interact with database
 class MongoDB {
   /* constructor to connect to the database
-    get the url from the environment variables provided by the .env file pass to server container
-    connect to the database and set the db property to the database name
+    - get the url from the environment variables or use the default url
+    - connect to the database and set the db property to the database name according to working environment
   */
   constructor() {
-    const url = process.env.MONGO_URL || 'mongodb://localhost:27017';
-    const envVar = process.env.NODE_ENV;
-    const dbName = envVar === 'test' ? 'test' : envVar === 'dev' ? 'codutopia' : dbName;
+    const url = process.env.MONGO_URL || 'mongodb://localhost:27017'; // URL of the database
+    const workEnv = process.env.NODE_ENV;
+    const dbName = workEnv === 'dev' ? 'dev' : 'codutopia' // database name according to working environment
+    const replicaSet = workEnv === 'dev' ? 'rs0' : 'codutopia'; // replica set name according to working environment
     this.db = mongoose
-      .connect(`${url}/${dbName}`)
+      .connect(`${url}/${dbName}`, { replicaSet })
       .then(() => {
         console.log('Connected to the mongoDB');
       })
@@ -23,19 +25,48 @@ class MongoDB {
         console.log('Error connecting to the database', err);
       });
   }
+
+  /* startSession method to start a session and start a transaction
+    Returns:
+      - the session object
+  */
+  async startSession() {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    return session;
+  }
+
+  /* abortTransaction method to abort a transaction and end the session
+    Parameters:
+      - session: session object to abort the transaction
+  */
+  async abortTransaction(session) {
+    await session.abortTransaction();
+    await session.endSession();
+  }
+
+  /* commitTransaction method to commit a transaction and end the session
+    Parameters:
+      - session: session object to commit the transaction
+  */
+  async commitTransaction(session) {
+    await session.commitTransaction();
+    await session.endSession();
+  }
 }
 
 // Create an instance of the MongoDB class
 const mongoDB = new MongoDB();
 
-// Create an instance of all models to interact with the database
+// Create an instance of all models classes to interact with each collection in the database
 const userModel = new UserModel();
 const lessonModel = new LessonModel();
 const quizModel = new QuizModel();
 const paymentModel = new PaymentModel();
+const courseModel = new CourseModel();
 
-// Export the mongoDB instance and all models
+
 export default mongoDB;
 export {
-  userModel, lessonModel, quizModel, paymentModel,
+  userModel, lessonModel, quizModel, paymentModel, courseModel,
 }

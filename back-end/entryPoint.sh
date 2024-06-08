64a6a6ba0004f447067613001ab23062335f7d7f
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-06-08 07:50:28 +0300

    Remove case of 'test' env and depend only on 'dev' one

--- back-end/entryPoint.sh
+++ back-end/entryPoint.sh
@@ -1,9 +1,7 @@
 #!/bin/env bash
 npm run generate-key
 if [ "$NODE_ENV" = "dev" ]; then
-  npm run start-watch
-elif [ "$NODE_ENV" = "test" ]; then
-  npm run test-full
+  npm run dev
 else
   npm run start
 fi

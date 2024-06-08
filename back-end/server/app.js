86603c12cf429cb71f447df6411902be63a76401
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-06-08 07:45:13 +0300

    Add new comments and add new middleware - Add morgan library to log the request and response - Remove cors ip whitelist - Add new comment to explain more about the code

--- back-end/server/app.js
+++ back-end/server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import userRouter from './routes/usersRoutes.js';
 import coursesRouter from './routes/coursesRoutes.js';
 import filesRouter from './routes/filesRoutes.js';
@@ -10,22 +11,19 @@ const app = express();
 // allow the server to accept JSON data
 app.use(express.json());
 // allow the server to accept requests from the front-end and set the credentials to true
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:4173', 'http://localhost:80',
-    'http://localhost', 'https://localhost', 'https://localhost:443'],
-  credentials: true
-}));
+app.use(cors());
 // set the headers to expose the token in the response header
 app.use((req, res, next) => {
   res.header('Access-Control-Expose-Headers', 'Authorization');
   next();
 });
+// use morgan for logging the requests to the console
+app.use(morgan('dev'));
 // use the userRouter for any request that starts with /users
 app.use('/users', userRouter);
-
 // use the coursesRouter for any request that starts with /courses
 app.use('/courses', coursesRouter);
-
+// use the filesRouter for any request that starts with /files
 app.use('/files', filesRouter);
 
 // start the server on port 3000

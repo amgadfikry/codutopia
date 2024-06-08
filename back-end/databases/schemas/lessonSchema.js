283645334ebc093e8b55ada03e5841738ce4c753
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-06-08 07:14:38 +0300

    Update schema with new fields and enchance class - Add two new fields (courseId, description) to lesson schema - Remove unnecessary documntaion and comments

--- back-end/databases/schemas/lessonSchema.js
+++ back-end/databases/schemas/lessonSchema.js
@@ -1,27 +1,24 @@
 import mongoose from "mongoose";
-// Save schema class from mongoose to a variable
 const Schema = mongoose.Schema;
 
 // Abstract class for lesson schema with pre and post hooks
 class LessonSchema {
 
   constructor() {
-    // Create a new schema for the lessons collection
+    // Define the schema of content in lesson
+    this.contentSchema = new Schema({
+      _id: false, // disable _id
+      title: { type: String, required: true },
+      type: { type: String, required: true, enum: ['text', 'video', 'image'] },
+      value: { type: String, required: true }
+    });
+
+    // Define the schema of lesson
     this.lessonSchema = new Schema({
-      // title: string value that is required
+      courseId: { type: String, ref: 'courses', required: true },
       title: { type: String, required: true, },
-      // content: list of content in lesson
-      content: [
-        {
-          // title: string value that is required
-          title: { type: String, required: true, },
-          // type: string value that is required
-          type: { type: String, required: true, },
-          // value: string value that is required
-          value: { type: String, required: true, },
-        },
-      ],
-      // timeToFinish: number value that is required
+      description: { type: String, required: true, },
+      content: [this.contentSchema], // array of content schema subdocuments
       timeToFinish: { type: Number, required: true, },
     }, { timestamps: true, }); // add timestamps to the schema
 

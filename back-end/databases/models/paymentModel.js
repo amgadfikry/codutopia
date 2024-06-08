1c5a0fc1f9e53ceea24c77107028558ce1e99b59
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-06-09 00:00:23 +0300

    Enhance method to be in transaction and improve it's code - Remove over documentation from the code - Add session parameters to each method to allow using it in transaction - Remove  unnecessary check if data pass to createPayment method is not in schema - Add check if query return null in each method and throw appropriate error message - Make checkPayment method to return false if not found payment instead of throw error - Make totalPayment method to return 0 if not found payment instead of throw error

--- back-end/databases/models/paymentModel.js
+++ back-end/databases/models/paymentModel.js
@@ -3,52 +3,66 @@ import PaymentSchema from "../schemas/paymentSchema.js";
 
 // PaymentModel class to interact with the payments collection in the database
 class PaymentModel extends PaymentSchema {
+
   constructor() {
-    // Call the parent class constructor
     super();
   }
 
   /* CreatePayment method to create a new payment in the database
     Parameters:
       - payment: object with the payment data
+      - session: optional session for the transaction
     Returns:
-      - the ID of the new payment
-      - error if the payment could not be created with specific message
+      - Payment object data of the created payment
+    Errors:
+      - Payment could not be created
+      - Missing required field
+      - Other errors
   */
-  async createPayment(payment) {
-    // check if payment object is contain invalid fields
-    const invalidFields = Object.keys(payment).filter(key => !Object.keys(this.paymentSchema.obj).includes(key));
-    // throw an error if the payment object contain invalid fields
-    if (invalidFields.length > 0) {
-      throw new Error(`Fields not in schema: ${invalidFields.join(', ')}`);
-    }
-
+  async createPayment(payment, session = null) {
     try {
+      // Define session in options object if it exists
+      const options = session ? { session } : {};
       // Create a new payment in the database
-      const newPayment = await this.payment.create(payment);
-      // Return the ID of the new payment
-      return newPayment._id;
-    } catch (error) {
-      // throw an error if the payment could not be created
-      throw new Error(`Missing ${Object.keys(error.errors)[0]} field`);
+      const newPayment = await this.payment.create([payment], options);
+      // if the payment could not be created, throw an error
+      if (!newPayment) {
+        throw new Error(`Payment could not be created`);
+      }
+      return newPayment[0];
+    }
+    catch (error) {
+      // If the error is a validation error, throw an error with the missing field
+      if (error.name === 'ValidationError') {
+        throw new Error(`Missing ${Object.keys(error.errors)[0]} field`);
+      } else {
+        throw error;
+      }
     }
   }
 
   /* GetPayment method to get a payment from the database
     Parameters:
       - paymentId: string value that represents the payment id
+      - session: optional session for the transaction
     Returns:
-      - the payment object
-      - error if the payment could not be found with specific message
+      - payment object data
+    Errors:
+      - Payment not found
   */
-  async getPayment(paymentId) {
+  async getPayment(paymentId, session = null) {
     try {
+      // Define session in options object if it exists
+      const options = session ? { session } : {};
       // Get the payment from the database
-      const result = await this.payment.findById(paymentId);
-      // Return the result object
+      const result = await this.payment.findById(paymentId, {}, options);
+      // if the payment is not found, throw an error
+      if (!result) {
+        throw new Error(`Payment not found`);
+      }
       return result;
-    } catch (error) {
-      // throw an error if the payment could not be found
+    }
+    catch (error) {
       throw new Error(`Payment not found`);
     }
   }
@@ -56,40 +70,50 @@ class PaymentModel extends PaymentSchema {
   /* CheckPayment method to check if a payment exists in the database
     Parameters:
       - paymentId: string value that represents the payment id
+      - session: optional session for the transaction
     Returns:
       - true if the payment exists
-      - error if the payment does not exist with specific message
+      - false if the payment does not exist
   */
-  async checkPayment(paymentId) {
+  async checkPayment(paymentId, session = null) {
     try {
+      // Define session in options object if it exists
+      const options = session ? { session } : {};
       // Get the payment from the database
-      const result = await this.payment.findById(paymentId);
-      // Return true if the payment exists
+      const result = await this.payment.findById(paymentId, {}, options);
+      if (!result) {
+        return false;
+      }
       return true;
     }
     catch (error) {
-      // throw an error if the payment does not exist
-      throw new Error(`Payment not found`);
+      return false;
     }
   }
 
   /* CourseTotalPayment method calculate the total payment amount for specific course
     Parameters:
       - courseId: string value that represents the course id
+      - session: optional session for the transaction
     Returns:
       - the total payment amount for the course
-      - error if not course found with specific message
+    Errors:
+      - Course not found
   */
-  async courseTotalPayment(courseId) {
+  async courseTotalPayment(courseId, session = null) {
     try {
       // Get the total payment amount for the course
       const totalPayment = await this.payment.aggregate([
         { $match: { courseId: courseId } },
         { $group: { _id: null, total: { $sum: "$paymentAmount" } } }
-      ]);
-      // Return the total payment amount
+      ]).session(session);
+      // if the course does not exist, throw an error
+      if (totalPayment.length === 0) {
+        throw new Error(`Course not found`);
+      }
       return totalPayment[0].total;
-    } catch (error) {
+    }
+    catch (error) {
       // throw an error if the course does not exist
       throw new Error(`Course not found`);
     }
@@ -97,19 +121,21 @@ class PaymentModel extends PaymentSchema {
 
   /* TotalPayment method calculate the total payment amount for all courses
     Parameters:
-      - None
+      - session: optional session for the transaction
     Returns:
       - the total payment amount for all courses
   */
-  async totalPayment() {
+  async totalPayment(session = null) {
     // Get the total payment amount for all courses
     const totalPayment = await this.payment.aggregate([
       { $group: { _id: null, total: { $sum: "$paymentAmount" } } }
-    ]);
-    // Return the total payment amount
+    ]).session(session);
+    // if there are no payments, return 0
+    if (totalPayment.length === 0) {
+      return 0;
+    }
     return totalPayment[0].total;
   }
 }
 
-// Export the PaymentModel class
 export default PaymentModel;

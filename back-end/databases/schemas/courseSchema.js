c26eb4f8592543e2138a441132b95ba00ae0014e
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-07-08 03:01:04 +0300

    Modify schema and add a new hook - Remove schema of subdocument of reviews and split it to isolated collection - Add tags field with validation to it - Modify reviews field to be an array of references to reviews collection - Add both courseAvgRating and sumReview fields to courseSchema with default value of 0 - Add pre hook before save to courseSchema to calculate the average rating of the course - Add static method to courseSchema to calculate the average rating of the course used in the pre hook

--- back-end/databases/schemas/courseSchema.js
+++ back-end/databases/schemas/courseSchema.js
@@ -5,29 +5,52 @@ const Schema = mongoose.Schema;
 class CourseSchema {
 
   constructor() {
-    // Define schema for the review subdocument in the courses collection
-    this.reviewSchema = new Schema({
-      _id: false, // disable _id for subdocument
-      userId: { type: String, ref: 'users', required: true, },
-      rating: { type: Number, required: true, range: [1, 5], },
-      comment: { type: String, default: null, },
-    }, { timestamps: true, }); // add timestamps to the schema 
-
     // Define schema for the courses collection
     this.courseSchema = new Schema({
       title: { type: String, required: true, },
       description: { type: String, required: true, },
+      // tags field with custom validation to check if the tags array has at least 3 tags
+      tags: {
+        type: [String], required: true,
+        validate: {
+          validator: (tags) => tags.length > 2,
+          message: 'Tags field must have at least 3 tags',
+        },
+      },
       authorId: { type: String, ref: 'users', required: true, },
       lessons: { type: [String], ref: 'lessons', default: [], },
       price: { type: Number, default: 0, },
       discount: { type: Number, default: 0, },
-      reviews: { type: [this.reviewSchema], default: [], }, // array of review subdocuments
+      reviews: { type: [String], ref: 'reviews', default: [], },
+      sumReviews: { type: Number, default: 0, },
+      courseAvgRating: { type: Number, default: 0, },
       students: { type: Number, default: 0, },
       image: { type: String, default: null, },
     }, { timestamps: true, }); // add timestamps to the schema
 
+    // Add pre hook to calculate the courseAvgRating before saving the course
+    this.courseSchema.pre('save', function (next) {
+      CourseSchema.calculateAvgCourseRating(this, next);
+    });
+
     // Create a new model for the courses collection with the course schema
     this.course = mongoose.model('courses', this.courseSchema);
+  }
+
+  /* calculateAvgCourseRating method to calculate the average rating for the course before saving
+    Parameters:
+      - course: course data object
+      - next: function to call the next middleware
+  */
+  static calculateAvgCourseRating(course, next) {
+    // check if length of reviews is less than 1 and return avg rating as 0
+    if (course.reviews.length < 1) {
+      course.courseAvgRating = 0;
+      return next();
+    }
+    // calculate the course average rating by dividing the sum of reviews by the number of reviews
+    course.courseAvgRating = course.sumReviews / course.reviews.length;
+    return next();
   }
 
 }

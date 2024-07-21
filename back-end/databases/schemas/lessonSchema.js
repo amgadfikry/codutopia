df928850e286fbf6f979d6bc063dcc307b30d2f1
amgadfikry <dr.amagd_sh92@yahoo.com>
2024-07-21 03:31:58 +0300

    Modify SChema of lessonSchema - Remove contentSchema subdocument to seprate schema and model - Make content field is list of ids of LessonContentModel model with default value is empty list

--- back-end/databases/schemas/lessonSchema.js
+++ back-end/databases/schemas/lessonSchema.js
@@ -5,20 +5,12 @@ const Schema = mongoose.Schema;
 class LessonSchema {
 
   constructor() {
-    // Define the schema of content in lesson
-    this.contentSchema = new Schema({
-      _id: false, // disable _id
-      title: { type: String, required: true },
-      type: { type: String, required: true, enum: ['text', 'video', 'image', 'pdf'] },
-      value: { type: String, required: true }
-    });
-
     // Define the schema of lesson
     this.lessonSchema = new Schema({
       title: { type: String, required: true, },
       courseId: { type: String, ref: 'courses', required: true, }, // course reference
       description: { type: String, required: true, },
-      content: { type: [this.contentSchema], required: true },
+      content: { type: [String], ref: 'lessonContents', default: [], }, // lesson content reference
       quiz: { type: String, ref: 'quizzes', default: null, }, // quiz reference
       timeToFinish: { type: Number, required: true, },
     }, { timestamps: true, }); // add timestamps to the schema

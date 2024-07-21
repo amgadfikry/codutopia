a47f93cd6a3dcd83e751961c07f2b16f9a2b8c44
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-07-21 03:34:38 +0300

    Modify lessonModel methods and add new methods - Modify all methods by define session in separte option and pass it directly in query - Change error messages in createLesson method - Create addContentToLesson method to add content id to lesson - Create removeContentFromLesson method to remove content id from lesson - Change getLesson method to getLessonWithContent to get lesson with content data - Pass run validator as option of query in updateLesson method and modify error messages - Change error messages in deleteLesson method - Enhance documentaion in all methods and make it more clear

--- back-end/databases/models/lessonModel.js
+++ back-end/databases/models/lessonModel.js
@@ -20,10 +20,8 @@ class LessonModel extends LessonSchema {
   */
   async createLesson(lesson, session = null) {
     try {
-      // Define session in options object if it exists
-      const options = session ? { session } : {};
       // Create a new lesson in the database
-      const newLesson = await this.lesson.create([lesson], options);
+      const newLesson = await this.lesson.create([lesson], { session });
       // if the lesson could not be created, throw an error
       if (!newLesson) {
         throw new Error(`Lesson could not be created`);
@@ -35,12 +33,71 @@ class LessonModel extends LessonSchema {
       if (error.name === 'ValidationError') {
         throw new Error(`Missing ${Object.keys(error.errors)[0]} field`);
       } else {
-        throw new Error(error.message);
+        throw new Error('Lesson could not be created');
       }
     }
   }
 
-  /* GetLesson method to get a lesson from the database
+  /* addContentToLesson method to add content to a lesson in the database
+    Parameters:
+      - lessonId: ID of the lesson to update
+      - contentId: ID of the content to add
+      - session: optional session for the transaction
+    Returns:
+      - message that the content is added to the lesson
+    Errors:
+      - Lesson not found
+  */
+  async addContentToLesson(lessonId, contentId, session = null) {
+    try {
+      // Update the lesson in the database
+      const result = await this.lesson.findByIdAndUpdate(
+        lessonId,
+        // Add the content to the lesson content array without duplicates
+        { $addToSet: { content: contentId } },
+        { new: true, session }
+      );
+      // if the lesson is not found, throw an error
+      if (!result) {
+        throw new Error(`Lesson not found`);
+      }
+      return 'Content added to the lesson successfully';
+    }
+    catch (error) {
+      throw new Error('Lesson not found');
+    }
+  }
+
+  /* removeContentFromLesson method to remove content from a lesson in the database
+    Parameters:
+      - lessonId: ID of the lesson to update
+      - contentId: ID of the content to remove
+      - session: optional session for the transaction
+    Returns:
+      - message that the content is removed from the lesson
+    Errors:
+      - Lesson not found
+  */
+  async removeContentFromLesson(lessonId, contentId, session = null) {
+    try {
+      // Update the lesson in the database
+      const result = await this.lesson.findByIdAndUpdate(
+        lessonId,
+        { $pull: { content: contentId } },
+        { new: true, session }
+      );
+      // if the lesson is not found, throw an error
+      if (!result) {
+        throw new Error(`Lesson not found`);
+      }
+      return 'Content removed from the lesson successfully';
+    }
+    catch (error) {
+      throw new Error('Lesson not found');
+    }
+  }
+
+  /* getLessonWithContent method to get a lesson from the database with content populated
     Parameters:
       - lessonId: ID of the lesson to get
       - session: optional session for the transaction
@@ -49,12 +106,10 @@ class LessonModel extends LessonSchema {
     Errors:
       - Lesson could not be found
   */
-  async getLesson(lessonId, session = null) {
+  async getLessonWithContent(lessonId, session = null) {
     try {
-      // Define session in options object if it exists
-      const options = session ? { session } : {};
       // Get the lesson from the database
-      const result = await this.lesson.findById(lessonId, {}, options);
+      const result = await this.lesson.findById(lessonId, {}, { session }).populate('content')
       // if the lesson is not found, throw an error
       if (!result) {
         throw new Error(`Lesson not found`);
@@ -75,25 +130,28 @@ class LessonModel extends LessonSchema {
       - Update lesson object data
     Errors:
       - Lesson not found
+      - Missing required field
   */
-  async updateLesson(lessonId, lesson, session = null) {
+  async updateLesson(lessonId, lessonData, session = null) {
     try {
-      // Define session in options object if it exists
-      const options = session ? { session } : {};
       // Update the lesson in the database
       const result = await this.lesson.findByIdAndUpdate(
         lessonId,
-        lesson,
-        { new: true, ...options }
+        lessonData,
+        { new: true, session, runValidators: true }
       );
       // if the lesson is not found, throw an error
       if (!result) {
-        throw new Error(`Lesson not found`);
+        throw new Error(`Failed to update lesson`);
       }
       return result;
     }
     catch (error) {
-      throw new Error('Lesson not found');
+      if (error.name === 'ValidationError') {
+        throw new Error(`Missing ${Object.keys(error.errors)[0]} field`);
+      } else {
+        throw new Error('Failed to update lesson');
+      }
     }
   }
 
@@ -161,18 +219,16 @@ class LessonModel extends LessonSchema {
   */
   async deleteLesson(lessonId, session = null) {
     try {
-      // Define session in options object if it exists
-      const options = session ? { session } : {};
       // Delete the lesson from the database
-      const result = await this.lesson.findByIdAndDelete(lessonId, options);
+      const result = await this.lesson.findByIdAndDelete(lessonId, { session });
       // if the lesson is not found, throw an error
       if (!result) {
-        throw new Error(`Lesson not found`);
+        throw new Error(`Lesson not found or could not be deleted`);
       }
       return "Lesson deleted successfully";
     }
     catch (error) {
-      throw new Error('Lesson not found');
+      throw new Error('Lesson not found or could not be deleted');
     }
   }
 }

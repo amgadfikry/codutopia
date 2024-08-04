b5eede5f01430901244cb6574c0b530db69e64c3
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-08-05 02:21:05 +0300

    Initialize the file of userService and import necessary packges and classes

---
+++ back-end/services/userService.js
@@ -0,0 +1,6 @@
+import mongoDB, { courseModel, userModel, reviewModel, lessonModel, lessonContentModel, quizModel, paymentModel }
+  from "../databases/mongoDB.js";
+import oracleStorage from "../oracleStorage/oracleStorage.js";
+import emailUtils from "../utils/emailUtils.js";
+
+

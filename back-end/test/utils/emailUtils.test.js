bdaae74ec3de440f78ae8d00ccfc914c922cc487
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-08-05 02:24:34 +0300

    Create test file for emailUtils with test suite for sendConfirmationMail method

---
+++ back-end/test/utils/emailUtils.test.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import emailUtils from '../../utils/emailUtils.js';
+
+
+// Test suite for the emailUtils module and its methods
+describe.skip('Unittest of EmailUtils', () => {
+
+  // Test suite for the sendConfirmationEmail method
+  describe('sendConfirmationEmail method', () => {
+    // Test case for sending a confirmation email with valid data and return a queued message
+    it('Send a confirmation email with valid data and return a queued message', async () => {
+      // call the sendConfirmationEmail method
+      const result = await emailUtils.sendConfirmationEmail('Amgad Fikry', 'dr.amgad_sh92@yahoo.com');
+      // assert the result
+      expect(result).have.property('id');
+      expect(result).have.property('message');
+      expect(result.message).equal('Queued. Thank you.');
+    });
+  });
+
+}).timeout(5000);
